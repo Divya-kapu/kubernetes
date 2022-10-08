cb061cee1fdac21118a926986853cb59f7c36352
Divya-kapu <98731187+Divya-kapu@users.noreply.github.com>
2022-10-08 02:02:17 -0700

    Update acceleration-calc.py

--- kubernetes_prometheus_monitoring/python-app/acceleration-calc.py
+++ kubernetes_prometheus_monitoring/python-app/acceleration-calc.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 _INF = float("inf")
 graphs = {}
 graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
-graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.',
-                        buckets=(1, 2, 5, 6, 10, _INF))
+#graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.',
+#                        buckets=(1, 2, 5, 6, 10, _INF))
 
 
 def dv_data_fetch_func(query_value_split1,sum,res_dct,path):
@@ -61,7 +61,7 @@ def main():
   graphs['c'].inc()
   time.sleep(0.600)
   end = time.time()
-  graphs['h'].observe(end - start)
+  #graphs['h'].observe(end - start)
   url = request.url
   url_response = calc_call_func(url)
   return url_response  #
